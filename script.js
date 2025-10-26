a4824e93f12014b8e8f54596caba49ceabf28199
FerdouseOfficial <mollajihad402@gmail.com>
2025-10-26 15:46:06 +0600

    actually fixing

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 let options = document.querySelectorAll("a");
 let h2 = document.querySelector("h2");
+let res-content = document.querySelector("main .container h2");
 
-let navbar = document.querySelector(".navbar");
+let navbar = document.querySelector("nav");
+let aside = document.querySelector("aside");
+// let acc = document.querySelector("h2");
+
+navbar.addEventListener("mouseover", () => {
+  h2.innerText = "Eat Fuchka";
+});
+
+navbar.addEventListener("mouseout", () => {
+  h2.innerText = "Fuchka";
+});
